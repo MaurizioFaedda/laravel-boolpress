2f8af36c2ca7fecb1ff23ac5cdd891d238c2bbf6
Maurizio <maurizio.faedda1@gmail.com>
2021-01-26 15:58:29 +0100

    Posts page for admin only

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.dashboard')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12">
+            <h1>Your blog posts</h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Slug</th>
+                        <th>Show</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($posts as $post)
+                        <tr>
+                            <td>{{ $post->id }}</td>
+                            <td>{{ $post->title }}</td>
+                            <td>{{ $post->slug }}</td>
+                            <td>
+                                <a class="btn btn-primary" href="#">
+                                    Show
+                                </a>
+                            </td>
+                            <td>
+                                <a class="btn btn-warning" href="#">
+                                    Edit
+                                </a>
+                            </td>
+                            <td>
+                                <form class="" action=""  method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger" name="button">
+                                    Delete
+                                </button>
+
+                            </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/guest/posts/index.blade.php
+++ resources/views/guest/posts/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>Tutti i post</h1>
+                <h1>Posts</h1>
                 <ul>
                     @foreach ($posts as $post)
                         <li>
