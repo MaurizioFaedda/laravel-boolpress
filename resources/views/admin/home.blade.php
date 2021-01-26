e339dc5ae716abf699d535f11c9199bc175092d0
Maurizio <maurizio.faedda1@gmail.com>
2021-01-26 21:11:02 +0100

    icon(only html for now) sidebar
    I want to create a single png with all the icons and then translate them (best practice) but for now I have printed the icons statically on html (bad practice).

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-
+        <div class="">
+            <h1>My Dashboard</h1>
             {{-- <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -15,6 +15,8 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <!-- Icon -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -22,12 +24,15 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md bg-wp shadow-sm sticky-top">
-            <div class="container-fluid">
+            <div class="container-fluid p-0">
                 <div class="rounded-circle d-flex align-items-center justify-content-center">
                     <a class="navbar-brand" href="{{ url('/') }}">
                         B
                     </a>
                 </div>
+                <a class="navbar-brand pl-2" href="{{ url('/') }}">
+                    Boolpress
+                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -71,7 +76,57 @@
                     <div class="sidebar-sticky">
                         <ul class="nav flex-column">
                             <li class="nav-item">
-                                <a class="nav-link active" href="{{ route('admin.posts.index') }}">Posts</a>
+                                <a class="nav-link active" href="{{ route('admin.index') }}"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+                            </li>
+                        </ul>
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ route('admin.posts.index') }}"><i class="fas fa-thumbtack"></i> Posts</a>
+                            </li>
+                        </ul>
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link active" href=""><i class="fas fa-icons"></i> Media</a>
+                            </li>
+                        </ul>
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link active" href=""><i class="fas fa-clone"></i> Pages</a>
+                            </li>
+                        </ul>
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link active" href=""><i class="fas fa-comment-alt"></i> Comments</a>
+                            </li>
+                        </ul>
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link active" href=""><i class="fas fa-file-contract"></i> WPForms</a>
+                            </li>
+                        </ul>
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link active" href=""><i class="fas fa-brush"></i> Appearance</a>
+                            </li>
+                        </ul>
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link active" href=""><i class="fas fa-plug"></i> Plugins</a>
+                            </li>
+                        </ul>
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link active" href=""><i class="fas fa-user"></i> Users</a>
+                            </li>
+                        </ul>
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link active" href=""><i class="fas fa-wrench"></i> Tools</a>
+                            </li>
+                        </ul>
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link active" href=""><i class="fas fa-sliders-h"></i> Settings</a>
                             </li>
                         </ul>
                 </nav>
