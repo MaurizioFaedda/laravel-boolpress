78daa6805166a74c5d03d6d49e068d8e7b7bf8b5
Maurizio <maurizio.faedda1@gmail.com>
2021-01-27 15:39:03 +0100

    populated category table with faker

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Category;
+use Illuminate\Support\Str;
+
+use Faker\Generator as Faker;
+
 
 class CategoriesTableSeeder extends Seeder
 {
@@ -9,8 +14,23 @@ class CategoriesTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 5 ; $i++) {
+            $new_category = new Category();
+            $new_category -> name = $faker->words(3, true);
+            $slug = Str::slug($new_category->name);
+            $slug_basic = $slug;
+            $new_slug = Category::where('slug', $slug)->first();
+            $counter = 1;
+            while($new_slug){
+               $slug = $slug_basic . '-' . $counter;
+               $counter++;
+               $new_slug = Category::where('slug', $slug)->first();
+            }
+            $new_category->slug = $slug;
+            $new_category->save();
+
+        }
     }
 }
