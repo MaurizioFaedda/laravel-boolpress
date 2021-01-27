cd96346951d2ebc8f9869a034bae73dcb1dc4844
Maurizio <maurizio.faedda1@gmail.com>
2021-01-27 18:45:31 +0100

    linked post.show category to category.show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -6,7 +6,7 @@
             <div class="col-6">
                 <h1>{{ $post->title }}</h1>
                 <p> {{ $post->content}}</p>
-                <p>Category: <a href="#">{{ $post->category ? $post->category->name : 'no category' }}</a></p>
+                <p>Category: <a href="{{ route('admin.categories.show', ['category' => $post->category_id]) }}">{{ $post->category ? $post->category->name : 'no category' }}</a></p>
             </div>
         </div>
     </div>
